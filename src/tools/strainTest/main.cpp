257aa2ba3d771f9bd9af571a92dcaab12c570800
Silvio Traversaro <pegua1@gmail.com>
2018-01-12 18:07:00 +0100

    strainTest skeleton

---
+++ src/tools/strainTest/main.cpp
@@ -0,0 +1,46 @@
+/**
+*
+@ingroup icub_tools
+\page strainTest strainTest: test boards based on IIT's STRAIN1, STRAIN1.5 and STRAIN2 boards
+
+Test boards based on IIT's STRAIN1, STRAIN1.5 and STRAIN2 boards
+
+\section intro_sec Description
+
+
+\section lib_sec Libraries
+YARP
+
+
+\section parameters_sec Parameters
+No parameters.
+
+\section portsa_sec Ports Accessed
+No ports accessed.
+
+\section portsc_sec Ports Created
+No ports created.
+
+\section tested_os_sec Tested OS
+Linux.
+
+\section example_sec Example Instantiation of the Module
+
+Just run:
+
+\code
+strainTest
+\endcode
+
+\author Silvio Traversaro
+
+Copyright (C) 2018 Silvio Traversaro
+ 
+CopyPolicy: Released under the terms of the GNU GPL v2.0.
+
+This file can be edited at src/strainTest/main.cpp
+**/
+
+int main(int argc, char *argv[])
+{
+}
